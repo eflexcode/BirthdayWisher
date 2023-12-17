a819c905d4125e4dd7be072ab72a455fa9b834ac
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2023-12-17 03:05:22 +0100

    Initial commit

---
+++ src/main/java/com/larrex/BirthdayWisher/BirthdayWisherApplication.java
@@ -0,0 +1,13 @@
+package com.larrex.BirthdayWisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayWisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayWisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larrex/BirthdayWisher/BirthdayWisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larrex.BirthdayWisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayWisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
